cd0c7d03bb7a208a0e05cfb9a9972a04f5ee8692
Järvinen Luukas <97948015+PocketRaven@users.noreply.github.com>
2024-04-10 14:01:01 +0300

    .

---
+++ Invaasio/Assets/Scripts/Alukset.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alukset : MonoBehaviour
+{
+
+    public Alukset[] prefabs;
+
+    public int rows = 15;
+
+    public int columns = 15;
+
+    private void Awake()
+    {
+        for (int row = 0; row < this.rows; row++)
+        {
+            
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
