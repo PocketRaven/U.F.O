4870b9d5162dc1a2020b371a2ef4655cf4e436b5
Luukas Järvinen <97948015+PocketRaven@users.noreply.github.com>
2024-05-04 02:57:51 +0300

    .

--- Invaasio/Assets/Scripts/UFO.cs
+++ Invaasio/Assets/Scripts/UFO.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class UFO : MonoBehaviour
 {
     public int hitPoints = 1; // Default hit points
+    public GameObject explosionPrefab; // Reference to the explosion prefab
 
     public void TakeDamage(int damage)
     {
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
+            Explode();
             Destroy(gameObject); // Destroy the UFO if hit points drop to 0 or below
+        }
+    }
+
+    void Explode()
+    {
+        if (explosionPrefab != null)
+        {
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 0.2f); // Destroy the explosion after 0.5 seconds
         }
     }
 }
